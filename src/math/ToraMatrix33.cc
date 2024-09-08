6d87885f375bfde0243df326563dc4c10051d44b
tora <www.wh1131457765wh@foxmail.com>
2024-09-08 19:13:42 +0800

    init matrix33

---
+++ src/math/ToraMatrix33.cc
@@ -0,0 +1,285 @@
+#include "ToraMatrix33.h"
+
+namespace Tora {
+
+// -----------------------------------------------------------------------------
+// Implementation of ToraMatrix33 functions
+// -----------------------------------------------------------------------------
+
+template <typename Real>
+ToraMatrix33<Real>::ToraMatrix33(const ChQuaternion<Real>& q) {
+  this->Set_A_quaternion(q);
+}
+
+template <typename Real>
+ToraMatrix33<Real>::ToraMatrix33(Real val) {
+  this->setZero();
+  this->diagonal().setConstant(val);
+}
+
+template <typename Real>
+ToraMatrix33<Real>::ToraMatrix33(const ToraVector3<Real>& v) {
+  this->setZero();
+  this->diagonal() = v.eigen();
+}
+
+template <typename Real>
+ToraMatrix33<Real>::ToraMatrix33(const ToraVector3<>& diag, const ToraVector3<>& off_diag) {
+  this->diagonal() = diag.eigen();
+
+  (*this)(0, 1) = off_diag.x();
+  (*this)(1, 0) = off_diag.x();
+
+  (*this)(0, 2) = off_diag.y();
+  (*this)(2, 0) = off_diag.y();
+
+  (*this)(1, 2) = off_diag.z();
+  (*this)(2, 1) = off_diag.z();
+}
+
+template <typename Real>
+ToraMatrix33<Real>::ToraMatrix33(Real angle, const ToraVector3<>& axis) {
+  ChQuaternion<Real> mr;
+  mr.Q_from_AngAxis(angle, axis);
+  this->Set_A_quaternion(mr);
+}
+
+template <typename Real>
+ToraMatrix33<Real>::ToraMatrix33(const ToraVector3<>& X, const ToraVector3<>& Y, const ToraVector3<>& Z) {
+  this->Set_A_axis(X, Y, Z);
+}
+
+template <typename Real>
+ToraVector3<Real> ToraMatrix33<Real>::operator*(const ToraVector3<Real>& v) const {
+  return ToraVector3<Real>((*this)(0, 0) * v.x() + (*this)(0, 1) * v.y() + (*this)(0, 2) * v.z(),
+                           (*this)(1, 0) * v.x() + (*this)(1, 1) * v.y() + (*this)(1, 2) * v.z(),
+                           (*this)(2, 0) * v.x() + (*this)(2, 1) * v.y() + (*this)(2, 2) * v.z());
+}
+
+template <typename Real>
+inline void ToraMatrix33<Real>::Set_A_quaternion(const ChQuaternion<Real>& q) {
+  Real e0e0 = q.e0() * q.e0();
+  Real e1e1 = q.e1() * q.e1();
+  Real e2e2 = q.e2() * q.e2();
+  Real e3e3 = q.e3() * q.e3();
+  Real e0e1 = q.e0() * q.e1();
+  Real e0e2 = q.e0() * q.e2();
+  Real e0e3 = q.e0() * q.e3();
+  Real e1e2 = q.e1() * q.e2();
+  Real e1e3 = q.e1() * q.e3();
+  Real e2e3 = q.e2() * q.e3();
+
+  (*this)(0, 0) = (e0e0 + e1e1) * 2 - 1;
+  (*this)(0, 1) = (e1e2 - e0e3) * 2;
+  (*this)(0, 2) = (e1e3 + e0e2) * 2;
+  (*this)(1, 0) = (e1e2 + e0e3) * 2;
+  (*this)(1, 1) = (e0e0 + e2e2) * 2 - 1;
+  (*this)(1, 2) = (e2e3 - e0e1) * 2;
+  (*this)(2, 0) = (e1e3 - e0e2) * 2;
+  (*this)(2, 1) = (e2e3 + e0e1) * 2;
+  (*this)(2, 2) = (e0e0 + e3e3) * 2 - 1;
+}
+
+template <typename Real>
+inline void ToraMatrix33<Real>::Set_A_Eulero(const ToraVector3<Real>& angles) {
+  Real cx = std::cos(angles.x());
+  Real cy = std::cos(angles.y());
+  Real cz = std::cos(angles.z());
+  Real sx = std::sin(angles.x());
+  Real sy = std::sin(angles.y());
+  Real sz = std::sin(angles.z());
+
+  (*this)(0, 0) = (cz * cx) - (cy * sx * sz);
+  (*this)(0, 1) = -(sz * cx) - (cy * sx * cz);
+  (*this)(0, 2) = sy * sx;
+  (*this)(1, 0) = (cz * sx) + (cy * cx * sz);
+  (*this)(1, 1) = -(sz * sx) + (cy * cx * cz);
+  (*this)(1, 2) = -sy * cx;
+  (*this)(2, 0) = sy * sz;
+  (*this)(2, 1) = sy * cz;
+  (*this)(2, 2) = cy;
+}
+
+template <typename Real>
+inline void ToraMatrix33<Real>::Set_A_Cardano(const ToraVector3<Real>& angles) {
+  Real cx = std::cos(angles.x());
+  Real cy = std::cos(angles.y());
+  Real cz = std::cos(angles.z());
+  Real sx = std::sin(angles.x());
+  Real sy = std::sin(angles.y());
+  Real sz = std::sin(angles.z());
+
+  (*this)(0, 0) = (cx * cz) - (sz * sx * sy);
+  (*this)(0, 1) = -sx * cy;
+  (*this)(0, 2) = (cx * sz) + (sx * sy * cz);
+  (*this)(1, 0) = (sx * cz) + (cx * sy * sz);
+  (*this)(1, 1) = cy * cx;
+  (*this)(1, 2) = (sx * sz) - (cx * sy * cz);
+  (*this)(2, 0) = -sz * cy;
+  (*this)(2, 1) = sy;
+  (*this)(2, 2) = cy * cz;
+}
+
+template <typename Real>
+inline void ToraMatrix33<Real>::Set_A_axis(const ToraVector3<Real>& X, const ToraVector3<Real>& Y,
+                                           const ToraVector3<Real>& Z) {
+  (*this)(0, 0) = X.x();
+  (*this)(0, 1) = Y.x();
+  (*this)(0, 2) = Z.x();
+  (*this)(1, 0) = X.y();
+  (*this)(1, 1) = Y.y();
+  (*this)(1, 2) = Z.y();
+  (*this)(2, 0) = X.z();
+  (*this)(2, 1) = Y.z();
+  (*this)(2, 2) = Z.z();
+}
+
+template <typename Real>
+inline void ToraMatrix33<Real>::Set_A_Xdir(const ToraVector3<Real>& Xdir, const ToraVector3<Real>& Vsingular) {
+  ToraVector3<Real> mX;
+  ToraVector3<Real> mY;
+  ToraVector3<Real> mZ;
+  Xdir.DirToDxDyDz(mX, mY, mZ, Vsingular);
+  this->Set_A_axis(mX, mY, mZ);
+}
+
+template <typename Real>
+inline ToraVector3<Real> ToraMatrix33<Real>::Get_A_Eulero() const {
+  ToraVector3<Real> eul;
+
+  eul.y() = std::acos((*this)(2, 2));                       // rho, nutation
+  eul.z() = std::acos((*this)(2, 1) / std::sin(eul.y()));   // csi, spin
+  eul.x() = std::acos(-(*this)(1, 2) / std::sin(eul.y()));  // rho, nutation
+
+  if (eul.y() == 0) {  // handle undefined initial position set
+    eul.x() = 0;
+    eul.z() = 0;
+  }
+
+  return eul;
+}
+
+template <typename Real>
+inline ToraVector3<Real> ToraMatrix33<Real>::Get_A_Cardano() const {
+  ToraVector3<Real> car;
+
+  Real mel21 = (*this)(2, 1);
+  if (mel21 > 1) mel21 = 1;
+  if (mel21 < -1) mel21 = -1;
+
+  car.y() = std::asin(mel21);
+
+  Real arg2 = (*this)(2, 2) / std::cos(car.y());
+  if (arg2 > 1) arg2 = 1;
+  if (arg2 < -1) arg2 = -1;
+  Real arg3 = (*this)(1, 1) / std::cos(car.y());
+  if (arg3 > 1) arg3 = 1;
+  if (arg3 < -1) arg3 = -1;
+
+  car.z() = std::acos(arg2);
+  car.x() = std::acos(arg3);
+
+  return car;
+}
+
+template <typename Real>
+inline ChQuaternion<Real> ToraMatrix33<Real>::Get_A_quaternion() const {
+  ChQuaternion<Real> q;
+  Real s, tr;
+  Real half = (Real)0.5;
+
+  Real m00 = (*this)(0, 0);
+  Real m01 = (*this)(0, 1);
+  Real m02 = (*this)(0, 2);
+  Real m10 = (*this)(1, 0);
+  Real m11 = (*this)(1, 1);
+  Real m12 = (*this)(1, 2);
+  Real m20 = (*this)(2, 0);
+  Real m21 = (*this)(2, 1);
+  Real m22 = (*this)(2, 2);
+
+  tr = m00 + m11 + m22;  // diag sum
+
+  if (tr >= 0) {
+    s = std::sqrt(tr + 1);
+    q.e0() = half * s;
+    s = half / s;
+    q.e1() = (m21 - m12) * s;
+    q.e2() = (m02 - m20) * s;
+    q.e3() = (m10 - m01) * s;
+  } else {
+    int i = 0;
+
+    if (m11 > m00) {
+      i = 1;
+      if (m22 > m11) i = 2;
+    } else {
+      if (m22 > m00) i = 2;
+    }
+
+    switch (i) {
+      case 0:
+        s = std::sqrt(m00 - m11 - m22 + 1);
+        q.e1() = half * s;
+        s = half / s;
+        q.e2() = (m01 + m10) * s;
+        q.e3() = (m20 + m02) * s;
+        q.e0() = (m21 - m12) * s;
+        break;
+      case 1:
+        s = std::sqrt(m11 - m22 - m00 + 1);
+        q.e2() = half * s;
+        s = half / s;
+        q.e3() = (m12 + m21) * s;
+        q.e1() = (m01 + m10) * s;
+        q.e0() = (m02 - m20) * s;
+        break;
+      case 2:
+        s = std::sqrt(m22 - m00 - m11 + 1);
+        q.e3() = half * s;
+        s = half / s;
+        q.e1() = (m20 + m02) * s;
+        q.e2() = (m12 + m21) * s;
+        q.e0() = (m10 - m01) * s;
+        break;
+    }
+  }
+
+  return q;
+}
+
+template <typename Real>
+inline ToraVector3<Real> ToraMatrix33<Real>::Get_A_Xaxis() const {
+  ToraVector3<Real> X;
+  X.x() = (*this)(0, 0);
+  X.y() = (*this)(1, 0);
+  X.z() = (*this)(2, 0);
+  return X;
+}
+
+template <typename Real>
+inline ToraVector3<Real> ToraMatrix33<Real>::Get_A_Yaxis() const {
+  ToraVector3<Real> Y;
+  Y.x() = (*this)(0, 1);
+  Y.y() = (*this)(1, 1);
+  Y.z() = (*this)(2, 1);
+  return Y;
+}
+
+template <typename Real>
+inline ToraVector3<Real> ToraMatrix33<Real>::Get_A_Zaxis() const {
+  ToraVector3<Real> Z;
+  Z.x() = (*this)(0, 2);
+  Z.y() = (*this)(1, 2);
+  Z.z() = (*this)(2, 2);
+  return Z;
+}
+
+template <typename Real>
+inline ToraVector3<Real> ToraMatrix33<Real>::GetAx() const {
+  return ToraVector3<Real>(0.5 * ((*this)(2, 1) - (*this)(1, 2)),  //
+                           0.5 * ((*this)(0, 2) - (*this)(2, 0)),  //
+                           0.5 * ((*this)(1, 0) - (*this)(0, 1)));
+}
+
+}  // namespace Tora
--- src/math/ToraMatrix33.h
+++ src/math/ToraMatrix33.h
@@ -7,4 +7,137 @@
 
 #include <cmath>
 
+#include "ToraMatrix.h"
+#include "ToraQuaternion.h"
+#include "ToraVector33.h"
+
+namespace Tora {
+
+/// Definition of a 3x3 fixed size matrix to represent 3D rotations and inertia tensors.
+template <typename Real = double>
+class ToraMatrix33 : public Eigen::Matrix<Real, 3, 3, Eigen::RowMajor> {
+ private:
+  // member in father class(Eigen::Matrix).
+
+ public:
+  /// Default constructor: uninitialized 3x3 matrix.
+  ToraMatrix33() : Eigen::Matrix<Real, 3, 3, Eigen::RowMajor>() {}
+
+  /// Constructor from Eigen expressions.
+  template <typename OtherDerived>
+  ToraMatrix33(const Eigen::MatrixBase<OtherDerived>& other) : Eigen::Matrix<Real, 3, 3, Eigen::RowMajor>(other) {}
+
+  /// Construct a diagonal matrix with the specified value on the diagonal.
+  ToraMatrix33(Real val);
+
+  /// Construct a diagonal matrix with the specified values on the diagonal.
+  ToraMatrix33(const ToraVector3<Real>& v);
+
+  /// Construct a symmetric 3x3 matrix with the specified vectors for the diagonal and off-digonal elements.
+  /// The off-diagonal vector is assumed to contain the elements A(0,1), A(0,2), A(1,2) in this order.
+  ToraMatrix33(const ToraVector3<>& diag, const ToraVector3<>& off_diag);
+
+  /// Construct a 3x3 rotation matrix from the given quaternion.
+  ToraMatrix33(const ToraQuaternion<Real>& q);
+
+  /// Construct a 3x3 rotation matrix from an angle and a rotation axis.
+  /// Note that the axis direction must be normalized.
+  ToraMatrix33(Real angle, const ToraVector3<>& axis);
+
+  /// Construct a 3x3 matrix with the given vectors as columns.
+  /// If the three vectors are mutually orthogonal unit vectors, the resulting matrix is a rotation matrix.
+  ToraMatrix33(const ToraVector3<>& X, const ToraVector3<>& Y, const ToraVector3<>& Z);
+
+  /// This method allows assigning Eigen expressions to ToraMatrix33.
+  template <typename OtherDerived>
+  ToraMatrix33& operator=(const Eigen::MatrixBase<OtherDerived>& other) {
+    this->Eigen::Matrix<Real, 3, 3, Eigen::RowMajor>::operator=(other);
+    return *this;
+  }
+
+  /// Allows multiplying a 3x3 matrix to other Eigen matrices.
+  using Eigen::Matrix<Real, 3, 3, Eigen::RowMajor>::operator*;
+
+  /// Multiply this matrix by a 3d vector.
+  ToraVector3<Real> operator*(const ToraVector3<Real>& v) const;
+
+  /// Fill this 3x3 matrix as a rotation matrix, given a unit quaternion.
+  void Set_A_quaternion(const ToraQuaternion<Real>& quat);
+
+  /// Fill this 3x3 matrix as a rotation matrix, given three Euler angles.
+  void Set_A_Eulero(const ToraVector3<Real>& angles);
+
+  /// Fill this 3x3 matrix as a rotation matrix, given three Cardano angles.
+  void Set_A_Cardano(const ToraVector3<Real>& angles);
+
+  /// Fill this 3x3 matrix as a rotation matrix, given the three versors X,Y,Z of the basis.
+  void Set_A_axis(const ToraVector3<Real>& X, const ToraVector3<Real>& Y, const ToraVector3<Real>& Z);
+
+  /// Fill this 3x3 matrix as a rotation matrix with the X axis along the provided direction.
+  /// Uses the Gram-Schmidt orthonormalization. The optional argument Vsingular, together with Xdir, suggests the XY
+  /// plane (as long as Xdir is not too close to lying in that plane, in which case a different direction is
+  /// selected).
+  void Set_A_Xdir(const ToraVector3<Real>& Xdir,                                   ///< X axis
+                  const ToraVector3<Real>& Vsingular = ToraVector3<Real>(0, 1, 0)  ///< suggested Y axis
+  );
+
+  /// Return the versor of X axis.
+  ToraVector3<Real> Get_A_Xaxis() const;
+
+  /// Return the versor of Y axis.
+  ToraVector3<Real> Get_A_Yaxis() const;
+
+  /// Return the versor of Z axis.
+  ToraVector3<Real> Get_A_Zaxis() const;
+
+  /// Return the corresponding unit quaternion.
+  /// Assumes that this is a rotation matrix.
+  ToraQuaternion<Real> Get_A_quaternion() const;
+
+  /// Return the Eulero angles.
+  /// Assumes that this is a rotation matrix.
+  ToraVector3<Real> Get_A_Eulero() const;
+
+  /// Assuming this matrix is a rotation matrix, get Ax vector.
+  ToraVector3<Real> GetAx() const;
+};
+
+}  // namespace Tora
+
+// -----------------------------------------------------------------------------
+
+/// Multiply a transposed 3x3 matrix with a vector.
+template <typename Real>
+ToraVector3<Real> operator*(const Eigen::Transpose<Eigen::Matrix<Real, 3, 3, Eigen::RowMajor>>& A,
+                            const ToraVector3<Real>& v) {
+  return ToraVector3<Real>(A(0, 0) * v.x() + A(0, 1) * v.y() + A(0, 2) * v.z(),
+                           A(1, 0) * v.x() + A(1, 1) * v.y() + A(1, 2) * v.z(),
+                           A(2, 0) * v.x() + A(2, 1) * v.y() + A(2, 2) * v.z());
+}
+
+/// Multiply a transposed const 3x3 matrix with a vector.
+template <typename Real>
+ToraVector3<Real> operator*(const Eigen::Transpose<const Eigen::Matrix<Real, 3, 3, Eigen::RowMajor>>& A,
+                            const ToraVector3<Real>& v) {
+  return ToraVector3<Real>(A(0, 0) * v.x() + A(0, 1) * v.y() + A(0, 2) * v.z(),
+                           A(1, 0) * v.x() + A(1, 1) * v.y() + A(1, 2) * v.z(),
+                           A(2, 0) * v.x() + A(2, 1) * v.y() + A(2, 2) * v.z());
+}
+
+/// Return the outer product (a 3x3 matrix) of two vectors.
+template <class Real>
+ToraMatrix33<Real> TensorProduct(const ToraVector3<Real>& vA, const ToraVector3<Real>& vB) {
+  ToraMatrix33<Real> T;
+  T(0, 0) = vA.x() * vB.x();
+  T(0, 1) = vA.x() * vB.y();
+  T(0, 2) = vA.x() * vB.z();
+  T(1, 0) = vA.y() * vB.x();
+  T(1, 1) = vA.y() * vB.y();
+  T(1, 2) = vA.y() * vB.z();
+  T(2, 0) = vA.z() * vB.x();
+  T(2, 1) = vA.z() * vB.y();
+  T(2, 2) = vA.z() * vB.z();
+  return T;
+}
+
 #endif
